478a556a34f307588703187c147840de3dd10a1d
Андрей Синицын <andrey-sin@yandex.ru>
2022-08-09 16:21:02 +0300

    fix api

--- frontend/src/store/actions/usersActions.js
+++ frontend/src/store/actions/usersActions.js
@@ -5,14 +5,14 @@ axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
 axios.defaults.xsrfCookieName = 'csrftoken'
 
 export const getUsers = () => dispatch => {
-    axios.get('/api/')
+    axios.get('/api/users/')
         .then(response => {
             dispatch(usersList(response.data));
         })
 }
 
 export const addUserAction = (user) => dispatch => {
-    axios.post('/api/', user)
+    axios.post('/api/users/', user)
         .then(response => {
             dispatch(addUser(response.data));
         })
